3647d0fd290a5be46de58b9c005848edbd1a7d81
fax1881 <zenalyoffical@gmail.com>
2025-07-11 19:21:49 +0300

    Güvenlik açıkları kapatıldı ve proje güvenli hale getirildi

--- next.config.js
+++ next.config.js
@@ -3,6 +3,31 @@ const nextConfig = {
   images: {
     domains: ['via.placeholder.com', 'images.unsplash.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig 
